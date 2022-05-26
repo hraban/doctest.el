7c2ebc98311e594fdc8ee40eab7fd511b086db62
Hraban Luyat <hraban@0brg.net>
2022-05-25 23:40:59 -0400

    WIP

--- doctest.el
+++ doctest.el
@@ -34,46 +34,54 @@
 (defun doctest--length (lst)
   "Return the length of list LST.
 
-  (doctest--length '())
-  ;; => 0
+(doctest--length '())
+=> 0
 
-  (doctest--length '(a))
-  ;; => 1
+(doctest--length '(a))
+=> 1
 
-  (doctest--length '(a b))
-  ;; => 2
+(doctest--length '(a b))
+=> 2
 "
   (length lst))
 
 (defun doctest--eval (expr)
-  (let* ((err nil)
-         (got (condition-case e
-                  (eval expr 'lexical)
-                (error (setq err e)))))
-    (list err got)))
+  (condition-case e
+      (list nil (eval (read-from-string expr) 'lexical))
+    (error (list e nil))))
+
+(defvar doctest--rex
+  (let ((sexp-line (rx bol "(" (* not-newline) ")" eol)))
+    (rx (group-n 1 (regexp sexp-line)
+                   (* "\n" (regexp sexp-line)))
+        (? "\n" (* " ") "->" (* " ") (group-n 2 (* not-newline)))
+        "\n" (* " ") "=>" (* " ") (group-n 3 (* not-newline))))
+  "Regular expression to match a doctest, including potential message output")
+
+(defun doctest--next-doctest ()
+  (when (search-forward-regexp doctest--rex nil t)
+    (mapcar (lambda (group)
+              ((match-string-no-properties '(1 2 3))))
+
+(defun doctest--collect-while-true (f)
+  "Call function F and collect its results while it returns a non-NIL value"
+  (cl-loop for x = (funcall f) while x collect x))
+
+(defun doctest--docstring-tests (docstring)
+  "Return tests in this DOCSTRING."
+  (with-temp-buffer
+    (insert docstring)
+    (goto-char (point-min))
+    (save-excursion
+      (save-match-data
+        (doctest--collect-while-true 'doctest--next-doctest)))))
 
 (defun doctest--function-tests (function)
   "Return tests in FUNCTION's docstring."
-  (let* ((docstring (documentation function 'raw))
-         (lines (and docstring (split-string docstring "\n" t)))
-         (i 0)
-         tests)
-    (while (< i (1- (length lines)))
-      (let ((line-1 (nth i lines))
-            (line-2 (nth (1+ i) lines)))
-        (cond
-         ((and (string-match (rx bol (* space) "(") line-1)
-               (string-match (rx bol (* space) ";; => ") line-2))
-          (push (cons
-                 ;; XXX handle read error?
-                 (read line-1)
-                 (read
-                  (substring line-2 (length (match-string 0 line-2)))))
-                tests)
-          (cl-incf i 2))
-         (t
-          (cl-incf i 1)))))
-    (nreverse tests)))
+  ;; this is dash’s -some-> in a nutshell
+  (let ((docstr (documentation function 'raw)))
+    (when docstr
+      (doctest--docstring-tests docstr))))
 
 (defun doctest--feature-functions (feature)
   (let (funcs)
@@ -97,22 +105,21 @@
              for i from 1
              for progress = (format "[%d/%d]" i total)
              do
-             (pcase test
-               (`(,expr . ,want)
-                (pcase (doctest--eval expr)
-                  (`(nil ,got)
-                   (cond
-                    ((equal got want)
-                     (push test passed)
-                     (message "%s %s pass" progress (car expr)))
-                    (t
-                     (let ((print-quoted t))
-                       (message "%s %s got: %s want: %s" progress expr got want)))))
-                  (`(,err ,_)
-                   (let ((print-quoted t))
-                     (message "%s %s got error: %s"
-                              progress
-                              expr (error-message-string err))))))))
+             (cl-destructuring-bind (expr msg want) test
+               (pcase (doctest--eval expr)
+                 (`(nil ,got)
+                  (cond
+                   ((equal got want)
+                    (push test passed)
+                    (message "%s %s pass" progress (car expr)))
+                   (t
+                    (let ((print-quoted t))
+                      (message "%s %s got: %s want: %s" progress expr got want)))))
+                 (`(,err ,_)
+                  (let ((print-quoted t))
+                    (message "%s %s got error: %s"
+                             progress
+                             expr (error-message-string err)))))))
     (message "Total: %d, Pass: %d, Fail: %d"
              total (length passed) (- total (length passed)))
     (list    total (length passed) (- total (length passed)))))
---
+++ run-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh -e
+
+# Mostly copied from
+# https://github.com/purcell/package-lint/blob/f16b8a131ff8443606b472aab4cefccb6ed66986/run-tests.sh
+
+NEEDED_PACKAGES="cl-lib dash"
+
+INIT_PACKAGE_EL="(progn \
+  (require 'package) \
+  (push '(\"melpa\" . \"https://melpa.org/packages/\") package-archives) \
+  (package-initialize) \
+  (unless package-archive-contents \
+     (package-refresh-contents)) \
+  (dolist (pkg '(${NEEDED_PACKAGES})) \
+    (unless (package-installed-p pkg) \
+      (package-install pkg))))"
+
+# Install packages
+emacs -Q -batch --eval "$INIT_PACKAGE_EL"
+
+
+# Byte compile
+emacs -Q --batch -L . --eval "(setq byte-compile-error-on-warn t)" -f batch-byte-compile doctest.el
+
+
+# Run doctests
+emacs -Q --batch -L . -l doctest f doctest-batch-check-feature doctest
